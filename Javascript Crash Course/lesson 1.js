1fbe0e06da9f9906c5b823da0d8632972dfd3510
Junaid Ali <64123692+junaidali1@users.noreply.github.com>
2020-12-27 11:33:53 +0500

    Add files via upload

---
+++ Javascript Crash Course/lesson 1.js
@@ -0,0 +1,14 @@
+console.log("hello");
+// alert("Junaid Ali");
+
+// Variables
+var b = "Smoothie";
+console.log(b);
+
+// Changing html's elements inner text with js
+document.getElementById("someText").innerHTML = "Junaid Ali";
+
+// asking user his name and print in html tag
+var p
+p = prompt("What is your name : ");
+document.getElementById("someText").innerHTML = p;
